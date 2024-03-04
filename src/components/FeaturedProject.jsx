2b9e6300760ddb5035e04f5d6b891352bc405750
barai3045 <santosh.barai.045@gmail.com>
2024-03-04 23:17:00 +0600

    first

---
+++ src/components/FeaturedProject.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturedProject = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FeaturedProject;
---
+++ src/components/SiteNavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SiteNavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SiteNavBar;
